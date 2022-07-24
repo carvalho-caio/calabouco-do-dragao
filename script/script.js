3bfcf00de2a2a14ea9c6f8bdf8586984bd30b1c3
Caio Carvalho <two_Caio@live.com>
2022-07-24 12:16:27 -0300

    teste

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
 //code below;
 
 const imagem = document.querySelector("#background");
+const logs = document.querySelector('#logs-game');
+const input = document.querySelector('#input-player');
+const botao = document.querySelector('#btn');
+
