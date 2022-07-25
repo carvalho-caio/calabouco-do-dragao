e49d8b09321e04d306724621b8e925788be25748
Caio Carvalho <two_Caio@live.com>
2022-07-25 12:49:21 -0300

    new func to add text to the logs area

--- script/script.js
+++ script/script.js
@@ -5,3 +5,13 @@ const logs = document.querySelector('#logs-game');
 const input = document.querySelector('#input-player');
 const botao = document.querySelector('#btn');
 
+const testando = document.querySelector('#testando');
+
+botao.addEventListener('click', () => {
+
+    let NovoTeste2 = testando.cloneNode(false);
+    NovoTeste2.innerHTML = input.value;
+
+    logs.appendChild(NovoTeste2);
+    input.value = "";
+})
