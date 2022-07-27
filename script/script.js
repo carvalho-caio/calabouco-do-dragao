eed2459d712280213426f7d30ceba5326eba07ea
Caio Carvalho <two_Caio@live.com>
2022-07-26 22:03:08 -0300

    func game over adicionada

--- script/script.js
+++ script/script.js
@@ -1,11 +1,15 @@
-//code below;
-
 const imagem = document.querySelector("#background");
 const logs = document.querySelector('#logs-game');
 const input = document.querySelector('#input-player');
 const botao = document.querySelector('#btn');
 const BotaoStart = document.querySelector('#start-game');
 const LogoScreen = document.querySelector('#game-start');
+
+//tela de game over;
+const BtnGameOver = document.querySelector('#btn-game-over');
+const GameOverScreen = document.querySelector('#game-over');
+
+
 
 const testando = document.querySelector('#testando');
 
@@ -15,7 +19,15 @@ botao.addEventListener('click', () => {
     NovoTeste2.innerHTML = input.value;
 
     logs.appendChild(NovoTeste2);
+
+    if(input.value == "game over")
+    {
+        GameOverScreen.classList.remove('esta-invisivel');
+    }
+
     input.value = "";
+
+    
 })
 
 BotaoStart.addEventListener('click', () => {
