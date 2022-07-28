b2fa8fa574fdf76bd9782064ba4e6726c3316809
Caio Carvalho <two_Caio@live.com>
2022-07-27 22:52:25 -0300

    adicionada funcao update, checagem de morte e funcao de troca src da vida

--- script/script.js
+++ script/script.js
@@ -5,6 +5,11 @@ const botao = document.querySelector('#btn');
 const BotaoStart = document.querySelector('#start-game');
 const LogoScreen = document.querySelector('#game-start');
 
+//vidas
+const Vidas = document.querySelector('#vidas')
+let QuantidadeDeVidas = 3;
+let isDead = false;
+
 //tela de game over;
 const BtnGameOver = document.querySelector('#btn-game-over');
 const GameOverScreen = document.querySelector('#game-over');
@@ -20,9 +25,9 @@ botao.addEventListener('click', () => {
 
     logs.appendChild(NovoTeste2);
 
-    if(input.value == "game over")
+    if(input.value == "vida")
     {
-        GameOverScreen.classList.remove('esta-invisivel');
+       QuantidadeDeVidas--;
     }
 
     input.value = "";
@@ -33,3 +38,45 @@ botao.addEventListener('click', () => {
 BotaoStart.addEventListener('click', () => {
     LogoScreen.classList.add("esta-invisivel");
 })
+
+//Função que faz o calculo de dano e o updte visual na vida do jogador;
+function Damage ()
+{
+    if(QuantidadeDeVidas == 3)
+    {
+        Vidas.src = "imagem/Vida-3.png";
+    } else if(QuantidadeDeVidas == 2)
+    {
+        Vidas.src = "imagem/Vida-2.png";
+    } else if(QuantidadeDeVidas == 1)
+    {
+        Vidas.src = "imagem/Vida-1.png";
+    } else if(QuantidadeDeVidas <= 0)
+    {
+        Vidas.src = "imagem/Vida-0.png";
+        isDead = true;
+    }
+}
+
+//Funcao que trata a morte do jogador;
+function EstaMorto()
+{
+    if(isDead)
+    {
+        GameOverScreen.classList.remove('esta-invisivel');
+    }
+}
+
+
+
+
+
+
+//Função de update do jogo;
+function UpdateGame()
+{
+    EstaMorto();
+    Damage();
+}
+
+setInterval(UpdateGame, 100);
