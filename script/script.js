daf173cc85c9a66f1dad78943e1b800e1caff24a
Caio Carvalho <two_Caio@live.com>
2022-08-07 21:48:52 -0300

    v0.1

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ const imagem = document.querySelector("#background");
 const LogoScreen = document.querySelector('#game-start');
 const BotaoStart = document.querySelector('#start-game');
 const GameText = document.querySelector('#game-text');
+const TelaVitoria = document.querySelector('#tela-vitoria');
 
 //Logs
 const logs = document.querySelector('#logs-game');
@@ -155,7 +156,7 @@ function Prologo()
 function FaseInicio(callback)
 {
 
-    MudarTextoPrincipal("Esse é o inicio de tudo, seu corno!");
+    MudarTextoPrincipal("Ao entrar no calabouço, você sente um arrepio descer sua espinha. Um sentimento ruim se abate sobre você, mas sua vontade não falha, e você decide continuar.");
     AdicionarLogDev("Comandos disponiveis:");
     AdicionarLogDev("Proximo (Avança para a proxima sala!)");
     AdicionarLogDev("Curar (Utiliza uma de suas poções para recuperar vida.)");
@@ -173,20 +174,272 @@ function FaseInicio(callback)
         {
             LogandoTexto();
             Cura();
-            FaseInicio();
+            FaseInicio(callback1);
         }
     }
 
     botao.addEventListener('click', callback1);
 }
 
+let TrapDamage = true;
+
 function FaseL1(callback)
 {
+    botao.removeEventListener('click', callback);
+
+    MudarTextoPrincipal("A luz da entrada já desapareceu completamente, você tropeça e cai sob cacos de vidro no chão, devivo a pouca visão. Você começa a escutar sussurros, que parecem vir das paredes...");
+
+    if(TrapDamage)
+    {
+        QuantidadeDeVidas--;
+        TrapDamage = false;
+    }
+
+    AdicionarLogDev("Comandos disponiveis:");
+    AdicionarLogDev("Proximo (Avança para a proxima sala!)");
+    AdicionarLogDev("Curar (Utiliza uma de suas poções para recuperar vida.)");
+    
     imagem.src = "imagem/teste2.jpg";
+
+    let callback2 = () => {
+        if(input.value == "Proximo")
+        {
+            LogandoTexto();
+            FaseL2(callback2);
+        } else if(input.value == "Curar")
+        {
+            LogandoTexto();
+            Cura();
+            FaseL1(callback2);
+        }
+    }
+
+    botao.addEventListener('click', callback2);
+}
+
+function FaseL2(callback)
+{
+    botao.removeEventListener('click', callback);
+
+    MudarTextoPrincipal("Você encontra uma bifurcação. Dois caminhos estão diante de você, mas qual é o correto?");
+    AdicionarLogDev("Comandos disponiveis:");
+    AdicionarLogDev("Esquerda (Avança pelo caminho da esquerda!)");
+    AdicionarLogDev("Direita (Avança pelo caminho da direita!)");
+    AdicionarLogDev("Curar (Utiliza uma de suas poções para recuperar vida.)");
+
+    imagem.src = "imagem/teste.jpg"
+
+    let callback3 = () => {
+        if(input.value == "Esquerda")
+        {
+            LogandoTexto();
+            FaseL3(callback3);
+        } else if(input.value == "Direita")
+        {
+            LogandoTexto();
+            FaseD1(callback3);
+        } else if(input.value == "Curar")
+        {
+            LogandoTexto();
+            Cura();
+            FaseL2(callback3);
+        }
+    }
+
+    botao.addEventListener('click', callback3);
+}
+
+function FaseD1(callback)
+{
+    botao.removeEventListener('click', callback);
+
+    MudarTextoPrincipal("Nessa sala você encontra um baú, mas será que é seguro abri-lo?");
+    AdicionarLogDev("Comandos disponiveis:");
+    AdicionarLogDev("Abrir (Tenta abrir o baú, mas pode haver armadilhas.)");
+    AdicionarLogDev("Proximo (Avança para a próxima sala, ignorando o baú)");
+
+    imagem.src = "imagem/teste2.jpg";
+
+    let callback6 = () => {
+        if(input.value == "Abrir")
+        {
+            LogandoTexto();
+            AdicionarLogDev("Poção encontrada!");
+            QuantidadePocao++;
+            FaseD2(callback6);
+        } else if(input.value == "Proximo")
+        {
+            LogandoTexto();
+            FaseD2(callback6);
+        }
+    }
+
+    botao.addEventListener('click', callback6);
+}
+
+function FaseD2(callback)
+{
+    botao.removeEventListener('click', callback);
+
+    MudarTextoPrincipal("Você encontra um inimigo! Um urso selvagem, enorme e com garras afiadas, parte pra cima de você!");
+    AdicionarLogDev("Comandos disponiveis:");
+    AdicionarLogDev("Atacar (Batalha com a criatura para avançar, podendo sofrer dano.)");
+    AdicionarLogDev("Fugir (Tenta avançar para a próxima fase fugindo da criatura, podendo sofrer dano.)");
+
+    imagem.src = "imagem/teste.jpg";
+
+    let callback7 = () => {
+        if(input.value == "Atacar")
+        {
+            LogandoTexto();
+            QuantidadeDeVidas = QuantidadeDeVidas - 2;
+            FaseT1(callback7);
+        } else if(input.value == "Fugir")
+        {
+            LogandoTexto();
+            QuantidadeDeVidas--;
+            FaseT1(callback7);
+        }
+    }
+
+    botao.addEventListener('click', callback7);
+}
+
+function FaseL3(callback)
+{
+    botao.removeEventListener('click', callback);
+
+    MudarTextoPrincipal("Você encontra um inimigo! Um lobo selvagem com os olhos vermelhos te encara, pronto para atacar!");
+    AdicionarLogDev("Comandos disponiveis:");
+    AdicionarLogDev("Atacar (Batalha com a criatura para avançar, podendo sofrer dano.)");
+    AdicionarLogDev("Fugir (Tenta avançar para a próxima fase fugindo da criatura, podendo sofrer dano.)");
+
+    imagem.src = "imagem/teste2.jpg";
+
+    let callback4 = () => {
+        if(input.value == "Atacar")
+        {
+            LogandoTexto();
+            FaseL4(callback4);
+        } else if(input.value == "Fugir")
+        {
+            LogandoTexto();
+            --QuantidadeDeVidas;
+            FaseL4(callback4);
+        }
+    }
+
+    botao.addEventListener('click', callback4);
+}
+
+function FaseL4(callback)
+{
+    botao.removeEventListener('click', callback);
+
+    MudarTextoPrincipal("Uma unica tocha ilumina a sala. Quase em completa escuridão, você escuta a voz pedindo socorro novamente, mas dessa vez, está mais perto de você.");
+    AdicionarLogDev("Comandos disponiveis:");
+    AdicionarLogDev("Proximo (Avança para a proxima sala!)");
+    AdicionarLogDev("Curar (Utiliza uma de suas poções para recuperar vida.)");
+
+    imagem.src = "imagem/teste.jpg";
+    
+    let callback5 = () => {
+        if(input.value == "Proximo")
+        {
+            LogandoTexto();
+            FaseT1(callback5);
+        } else if(input.value == "Curar")
+        {
+            LogandoTexto();
+            Cura();
+            FaseL4(callback5);
+        }
+    }
+
+    botao.addEventListener('click', callback5);
+}
+
+function FaseT1(callback)
+{
+    botao.removeEventListener('click', callback);
+
+    MudarTextoPrincipal("Você se encontra em uma sala pequena, com uma enorme porta a frente.");
+    AdicionarLogDev("Comandos disponiveis:");
+    AdicionarLogDev("Proximo (Avança para a proxima sala!)");
+    AdicionarLogDev("Curar (Utiliza uma de suas poções para recuperar vida.)");
+    
+    imagem.src = "imagem/teste2.jpg";
+
+    let callback8 = () => {
+        if(input.value == "Proximo")
+        {
+            LogandoTexto();
+            FaseT2(callback8);
+        } else if(input.value == "Curar")
+        {
+            LogandoTexto();
+            Cura();
+            FaseT1(callback8);
+        }
+    }
+
+    botao.addEventListener('click', callback8);
+}
+
+function FaseT2(callback)
+{
+    botao.removeEventListener('click', callback);
+
+    MudarTextoPrincipal("Um enorme corredor está a sua frente, com pilares negros e poucas tochas iluminando seu caminho. Você tem a sensação de que algo terrivel te espera...");
+    AdicionarLogDev("Comandos disponiveis:");
+    AdicionarLogDev("Proximo (Avança para a proxima sala!)");
+    AdicionarLogDev("Curar (Utiliza uma de suas poções para recuperar vida.)");
+
+    imagem.src = "imagem/teste.jpg";
+
+    let callback9 = () => {
+        if(input.value == "Proximo")
+        {
+            LogandoTexto();
+            FaseB0(callback9);
+        } else if(input.value == "Curar")
+        {
+            LogandoTexto();
+            Cura();
+            FaseT2(callback9);
+        }
+    }
+
+    botao.addEventListener('click', callback9);
+}
+
+function FaseB0(callback)
+{
+    botao.removeEventListener('click', callback);
+
+    MudarTextoPrincipal("Você encontra um enorme Dragão, fazendo uma donzela de refém! A fera enorme e assutadora rugi e abre as asas quando te vê!");
+    AdicionarLogDev("Comandos disponiveis:");
+    AdicionarLogDev("Atacar (Tenta combater o Dragão!)");
+    AdicionarLogDev("Fugir (Tenta pegar a donzella e fugir do Dragão!)");
+
+    imagem.src = "imagem/teste2.jpg";
+
+    let finalcallback = () => {
+        if(input.value == "Atacar")
+        {
+            LogandoTexto();
+            TelaVitoria.classList.remove('esta-invisivel');
+        } else if(input.value == "Fugir")
+        {
+            LogandoTexto();
+            QuantidadeDeVidas = 0;
+        }
+    }
+
+    botao.addEventListener('click', finalcallback);
 }
 
 Prologo();
-
 
 //Função de update do jogo;
 function UpdateGame()
@@ -195,8 +448,5 @@ function UpdateGame()
     Damage();
     UpdatePotion();
 }
-
-
-
 
 setInterval(UpdateGame, 10);
